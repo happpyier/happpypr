a4b159edd861e62a5b70a4e9e906de79580e7027
Thomas Bernfeld <happpyier@gmail.com>
2016-04-08 13:07:55 -0400

    setting up the voting app

--- index.js
+++ index.js
@@ -8,9 +8,6 @@ app.set("Content-Type", "text/html");
 app.get('/', function(request, response) {
   response.sendFile(path.join(__dirname+'/index.html'));
 });
-app.get('/voting', function(request, response) {
-  response.sendFile(path.join(__dirname+'/voting/index.js'));
-});
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port')); 
 });
