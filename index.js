e16540cb7794bb0b12aa28d892b9a1f150fe7645
Thomas Bernfeld <happpyier@gmail.com>
2016-04-22 14:09:15 -0400

    From Work Trying DB stuff out

--- index.js
+++ index.js
@@ -20,7 +20,19 @@ app.set("Content-Type", "text/html");
 	  response.sendFile(path.join(__dirname+'/voting/polls.html'));
 	});
 	app.get('/voting/polls/:id', function(request, response) {
-	  response.sendFile(path.join(__dirname+'/voting/thispoll.html'));
+		pg.connect(process.env.DATABASE_URL, function(err, client, done) 
+		{
+			client.query("SELECT * FROM vote_tb", function(err, result) {
+			  if (err)
+			   //{ resultsSQL = "Error "+ err; response.send("Error " + err);  }
+			   { resultsidSQL = ("Error " + err); }
+			  else
+			   //{ resultsSQL = "Results " + {results: result.rows}; response.render('pages/db', {results: result.rows} ); }
+			   { resultsidSQL = JSON.stringify(result.rows); }
+			   done();
+			});	
+		});
+	  response.sendFile(path.join(__dirname+'/voting/thispoll.html?vals='+resultsidSQL));
 	});
 	app.get('/voting/mypolls', function(request, response) {
 	  response.sendFile(path.join(__dirname+'/voting/mypolls.html'));
