5cb925f4ed71f14b74d5e701b84e95e3444ab755
Thomas Bernfeld <happpyier@gmail.com>
2016-10-10 15:23:35 -0400

    Fresh Start

--- index.js
+++ index.js
@@ -36,7 +36,8 @@ app.get('', function(request, response) {
 		    {
 				
 				resultsidSQL = JSON.stringify(Object.keys(result));
-				var rowCount = parseInt(JSON.stringify(result.rowCount));
+				var prerowCount = parseInt(JSON.stringify(result.rowCount));
+				var rowCount = parseInt(prerowCount);
 				//for (var i=0; i < rowCount; i++)
 				//{
 				//	response.write(JSON.stringify(Object.keys(result.rows));
