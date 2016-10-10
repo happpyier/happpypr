02882b7067d3028e7dbd36b8ee6c5d039c63d0f4
Thomas Bernfeld <happpyier@gmail.com>
2016-10-10 16:29:05 -0400

    Fresh Start

--- index.js
+++ index.js
@@ -40,7 +40,9 @@ app.get('', function(request, response) {
 				var resultsidSQL = preresultsidSQL.split(",");
 				//response.write(typeof(resultsidSQL) + "<br/>" + resultsidSQL);
 				resultsidSQL.forEach(function(value){
-						response.write(value['randid'] + "<br/>");
+					value.forEach(function(innerValue){
+						response.write(innerValue['randid'] + "<br/>");
+					});	
 				});
 				//resultsidSQL.forEach(function(entry) {
 				//	response.write(entry);
