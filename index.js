37c14dde0a751bbcf3f53993a6628adcaf0e58b5
Thomas Bernfeld <happpyier@gmail.com>
2016-10-12 14:58:13 -0400

    Lets doing some submitting

--- index.js
+++ index.js
@@ -95,10 +95,6 @@ app.get('/mypolls', function(request, response)
 		response.end(data);
 	});	
 });
-app.get('/returnfalse', function(request, response)
-{
-	return false;
-});
 app.get('/newpoll', function(request, response)
 {
 	fs.readFile('newpoll.html', 'utf8', function (err,data) 
