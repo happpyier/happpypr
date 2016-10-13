55cec54612ba677a3ad77cae1ded08846f249d82
Thomas Bernfeld <happpyier@gmail.com>
2016-10-13 10:21:22 -0400

    Working on the sql

--- index.js
+++ index.js
@@ -114,8 +114,8 @@ app.get('/submit/:id/:selection', function(request, response)
 	var postSqlVar1 = "UPDATE vote_tb  SET votedalready = '1' WHERE ipvoted LIKE \'"+clientIP+"\'";
 	var postSqlVar2 = "UPDATE vote_tb  SET votes = votes+1 WHERE votechoose = \'"+selectionVar+"\'";
 	var location = '/polls/' + pickId;
-	response.write (postSqlVar1 + "<br/>" + postSqlVar2);
-	response.end();
+	//response.write (postSqlVar1 + "<br/>" + postSqlVar2);
+	//response.end();
 	pg.connect(process.env.DATABASE_URL, function(err, client, done) 
 	{
 		client.query(postSqlVar1, function(err, result) 
