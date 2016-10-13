8c00a2915ce32da579450ef64dfa420a11be4bd6
Thomas Bernfeld <happpyier@gmail.com>
2016-10-13 09:01:33 -0400

    Working on the sql

--- index.js
+++ index.js
@@ -71,13 +71,13 @@ app.get('/polls/:id', function(request, response) {
 				alertVar = result.rows;
 				alertVar.forEach(function(value)
 				{
-					var randid_vote += value["randid"];
-					var votechoose_vote += value["votechoose"];
-					var votes_vote += value["votes"];
-					var uservoted_vote += value["uservoted"];
-					var ipvoted_vote += value["ipvoted"];
-					var title_vote += value["title"];
-					var votedalready += value["votedalready"];
+					var randid_vote = randid_vote + value["randid"];
+					var votechoose_vote = value["votechoose"];
+					var votes_vote = value["votes"];
+					var uservoted_vote = value["uservoted"];
+					var ipvoted_vote = value["ipvoted"];
+					var title_vote = value["title"];
+					var votedalready = value["votedalready"];
 				});
 				response.write( "<div>" + alertVar + "</div><div class='hidden' style='display:none' id= 'randid_hidden'>" + value["randid"] + "</div> <div class='hidden' style='display:none' id= 'votechoose_hidden'>" + value["votechoose"] + "</div> <div class='hidden' style='display:none' id= 'votes_hidden'>" + value["votes"] + "</div> <div class='hidden' style='display:none' id= 'uservoted_hidden'>" + value["uservoted"] + "</div> <div class='hidden' style='display:none' id= 'ipvoted_hidden'>" + value["ipvoted"] + "</div> <div class='hidden' style='display:none' id= 'title_hidden'>" + value["title"] + "</div> <div class='hidden' style='display:none' id= 'alreadyvoted'>" + value["alreadyvoted"] + "</div>"	);
 		   }
