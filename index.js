36b76e14804563b233b97e662ea681767ddccb91
Thomas Bernfeld <happpyier@gmail.com>
2016-10-26 16:03:36 -0400

    Twitter Work

--- index.js
+++ index.js
@@ -181,7 +181,7 @@ app.get('/info', function(request, response)
 		response.end(data);
 	});	
 });
-app.get('/twitter/auth oauth/authorize' , function(request, response)
+app.get('/twitter/auth' , function(request, response)
 {
 	var Authorization = 'oauth_consumer_key="WVpvQlZJOUFrMk1BeExUUko0NjBjNjVPcQ=="&oauth_version="1.0"';
 	var location = '//https://api.twitter.com/oauth/request_token?' + Authorization;
