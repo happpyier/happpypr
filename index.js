1955a4784c3e264c10615c070e75c9efd32101ad
Thomas Bernfeld <happpyier@gmail.com>
2016-10-31 09:59:02 -0400

    Twitter still at work

--- index.js
+++ index.js
@@ -192,25 +192,21 @@ app.get('/twitter/auth' , function(req, res)
 	*/
 		var options = 
 	{
-		hostname: 'www.postcatcher.in',
 		port: 80,
-		path: '/catchers/544b09b4599c1d0200000289',
 		method: 'POST',
 		headers: 
 		{
 		  'Content-Type': 'application/json',
 		}
 	};
-	var req = http.request(options, function(res) 
-	{
-	  console.log('Status: ' + res.statusCode);
-	  console.log('Headers: ' + JSON.stringify(res.headers));
-	  res.setEncoding('utf8');
-	  res.on('data', function (body) 
-	  {
-		console.log('Body: ' + body);
-	  });
-	});
+	//var req = http.request(options, function(res) 
+	//{
+	//  res.setEncoding('utf8');
+	//  res.on('data', function (body) 
+	//  {
+	//	console.log('Body: ' + body);
+	//  });
+	//});
 	req.on('error', function(e) {
 	  console.log('problem with request: ' + e.message);
 	});
