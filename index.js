bd055520d00a544a00e91abe80c118711b52d22c
Thomas Bernfeld <happpyier@gmail.com>
2016-10-31 12:11:49 -0400

    Twitter still at work

--- index.js
+++ index.js
@@ -198,7 +198,7 @@ app.get('/twitter/auth' , function(request, response)
     console.log('==>Get the request token');
     console.log(arguments);
 	console.log("it worked yeah");
-	response.write(typeof(arguments));
+	response.write(Object.keys(arguments));
 	response.end();
 	});
 	//response.write(testVar);
