98538a942ea6184f98b1557c3af8fd9452a5d841
Thomas Bernfeld <happpyier@gmail.com>
2016-11-01 10:54:45 -0400

    Twitter Authorizing

--- index.js
+++ index.js
@@ -180,8 +180,8 @@ app.get('/info', function(request, response)
 	});	
 });
 var twitter = new Twitter({
-	consumerKey: process.env.TWITTER_CONSUMER_KEY,
-	consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+	consumerKey: 'YZoBVI9Ak2MAxLTRJ460c65Oq',
+	consumerSecret: 'UxkG05HcRBlOmOVLvcHM9AlFStHStUMKwtuCKXM0nwtbm5IJAP'
 });
 var _requestSecret;
 app.get("/twitter/auth", function(req, res) {
