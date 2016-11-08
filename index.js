ebc9c45d78230bdd6beb27f98da1da7410e71593
Thomas Bernfeld <happpyier@gmail.com>
2016-11-08 12:58:17 -0500

    Final Steps

--- index.js
+++ index.js
@@ -355,11 +355,12 @@ app.get('/newpoll/submit/:randid/:title/:votechoose', function(request, response
 		Almost_clientUser = pre_clientUser.match(rePattern);
 		_clientUser = Almost_clientUser[1];
 		Almost_pickVotechoose = prepickVotechoose.split("|");
-		var pickVotechoose = Almost_pickVotechoose[0];
+		var pickVotechoose = Almost_pickVotechoose;
 		for(var i = 0; i<pickVotechoose.length; i++) 
 		{
 			response.write("Test" + pickVotechoose[i]);
 		}
+		
 		response.end();
 		/*
 		for(var i = 0; i<pickVotechoose.length; i++) 
